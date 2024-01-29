e9a58b4a1fdc8f1ef100ad1da6dce8aff503dc64
pgarchar <pgarchar@gmail.com>
2024-01-29 14:30:21 -0500

    waitlist pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,85 +2,39 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Testing changes!!! editing &nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
+      return (
+        <div className={styles.container}>
+            <Head>
+                <title>Waitlist</title>
+                <meta name="description" content="A quick, scalable waitlist"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://docs.amplify.aws/gen2/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/start/quickstart/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Quickstart <span>-&gt;</span>
-          </h2>
-          <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Auth <span>-&gt;</span>
-          </h2>
-          <p>Zero-config Auth UI components with social sign-in and MFA.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Data <span>-&gt;</span>
-          </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            // New components
+            <LeftSide/>
+            <RightSide/>
+        </div>
+    )
 }
+
+// These functions can be moved into their own files
+function LeftSide() {
+    return <div className={styles.column}>
+        <img width="154" height="27" src="/logo.svg"/>
+        <h1 className={styles.title}>
+            Quick Scalable<br/>
+            <span className={styles.titleKeyword}>Waitlist</span>
+        </h1>
+        <div className={styles.subtitle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua.
+        </div>
+    </div>
+}
+
+function RightSide() {
+    return <div className={styles.column}>
+        <img width="100%" height="100%" src="/code.svg"/>
+    </div>
+}
+  
