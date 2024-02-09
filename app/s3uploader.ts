c8b2d7b77026cc20dacfff6cb5e681d6235b08a9
pgarchar <pgarchar@gmail.com>
2024-02-09 12:22:13 -0500

    test33

--- app/s3uploader.ts
+++ app/s3uploader.ts
@@ -1,3 +1,6 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+
 import { S3 } from 'aws-sdk';
 
 // Replace these values with your S3 bucket details
@@ -6,6 +9,7 @@ const region = 'us-east-1';
 const accessKeyId = 'AKIA25TNZ25HENZVDMB2';
 const secretAccessKey = 'GF/XyCd83jHHdaj1uZcwN3QvDFuab5ZQHSlrx9yQ';
 
+const client = new S3Client({});
 
 const s3 = new S3({
   region,
@@ -13,6 +17,26 @@ const s3 = new S3({
   secretAccessKey,
 });
 
+interface UploadParams {
+  key: string;
+  data: Record<string, any>;
+}
+
+
+export const main = async () => {
+  const command = new PutObjectCommand({
+    Bucket: "test-bucket",
+    Key: "hello-s3.txt",
+    Body: "Hello S3!",
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+};
 interface UploadParams {
   key: string;
   data: Record<string, any>;
@@ -35,3 +59,4 @@ export const uploadToS3= async ({ key, data }: UploadParams) => {
     throw error;
   }
 };
+
